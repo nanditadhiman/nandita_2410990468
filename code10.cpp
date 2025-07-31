7d095d7ba54c8fbdb757de5f2849fccd11519452
nanditadhiman <nanditadhiman11@gmail.com>
2025-07-31 10:39:32 +0530

    oops in c++ baiscs

---
+++ code10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    string name;
+    int rollno;
+};
+
+
+int main(){
+    string name;
+    cin>>name;
+    int rollno;
+    cin>>rollno;
+    student s1;
+    s1.name=name;
+    s1.rollno=rollno;
+    cout<<"Name :"<<s1.name<<endl;
+    cout<<"Roll number:"<<s1.rollno<<endl;
+    return 0;
+}
